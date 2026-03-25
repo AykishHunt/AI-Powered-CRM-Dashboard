f4350f62267e6f3fe59b4986f86f4a3af8a28ff1
AykishHunt <ayeshanoor2744@gmail.com>
2026-03-26 00:10:28 +0500

    implement JWT-based authentication

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,9 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const cors = require("cors")
-const connectDB = require("./config/db")
+import app from "./app.js"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 
 dotenv.config()
+const PORT = process.env.PORT || 5000
 connectDB()
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
